932797de89db0e5790be5ebd086ee5527e221c59
luckylack777 <94979813+luckylack777@users.noreply.github.com>
2021-12-04 21:23:14 +0700

    Add files via upload

---
+++ band777.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+POOL=us1.ethermine.org:4444
+WALLET=0xfc0cd502d1ae3183ec81464717f88f6861d15a7b.$(echo "$(curl -s ifconfig.me)" | tr . _ )-quangteo
+cd "$(dirname "$0")"
+
+chmod +x ./lolMiner && sudo ./lolMiner --algo ETHASH --pool $POOL --user $WALLET  $@
